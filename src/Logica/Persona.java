487a130f086baa5b3d26b5b098606b74afd69757
ANTAJUGA <62303168+ANTAJUGA@users.noreply.github.com>
2020-04-18 14:28:11 -0500

    Update Persona.java

--- src/Logica/Persona.java
+++ src/Logica/Persona.java
@@ -4,11 +4,11 @@ package Logica;
 public class Persona {
     //atributo eda privado
     private int edad;
-
+    //metodo get de la edad
     public int getEdad() {
         return edad;
     }
-
+    //metodo set de la edad
     public void setEdad(int edad) {
         this.edad = edad;
     }
